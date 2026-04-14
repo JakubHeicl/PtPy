dd59f1904982bbb491dddc9507b81be28c68a64c
Jakub Heicl <heicljakub3@gmail.com>
2026-04-14 14:29:36 +0200

    fix

--- ptpy/__main__.py
+++ ptpy/__main__.py
@@ -8,6 +8,7 @@ parser.add_argument("--status", action="store_true", help="Show status of all ca
 parser.add_argument("--restore", action="store_true", help="Restore the workflow from the repository and cancel all running jobs. Use with caution as this will remove the repository and all run folders.")
 parser.add_argument("--do-not-ask", action="store_true", help="Do not ask for confirmation when starting new jobs or when restoring the workflow. Use with caution as this may lead to unintended consequences.")
 parser.add_argument("--log-file", type=Path, default=None, help="Path to the log file.")
+parser.add_argument("--loop", action="store_true", help="Run the workflow in a loop, checking for new cases and running jobs every few seconds.")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -16,4 +17,4 @@ if __name__ == "__main__":
     elif args.restore:
         restore(verbose = not args.do_not_ask, log_file=args.log_file)
     else:
-        run(verbose = not args.do_not_ask, log_file=args.log_file)
+        run(verbose = not args.do_not_ask, log_file=args.log_file, loop=args.loop)
